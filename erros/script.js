fcfce855cf38989a28130ced951002b357ebd6a7
leonardorledesma <leo.ledesma@gmail.com>
2022-04-17 11:05:51 -0300

    primeiro commit -  anotações de aula e atividade exemplo.

---
+++ erros/script.js
@@ -0,0 +1,7 @@
+/* ECMAScript error é um erro que acontece em tempo de execução.
+compost por mensagem, nome do erro, a linha onde ocorreu e o call stack
+que é a pilha de chamadas até o erro.
+
+DOMException é um erro do DOM document object model são erros de quando
+estamos relacionando o nosso codigo java script a uma pagina de web.
+*/
---
+++ throw try catch finally/script.js
@@ -0,0 +1,10 @@
+/* Throw
+    a diferença entre throw e return é que o return vai retornar o que você declarou
+    como por exemplo uma string, ja quando se utiliza o throw retorna de fato o erro que ocorreu.
+
+Try/Cacth
+    dentro do bloco try vai ser verificado se há um erro, caso ocorra será capturado pela
+    declaração cacth podendo ser manipulado da maneira que quisermos.
+
+Finally
+    Bloca de código que é executado independente de ter ocorrido erro ou não.*/
